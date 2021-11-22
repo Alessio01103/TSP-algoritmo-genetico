840a6bc0dbb289752a98f9a99f79e7596c2bf976
Alessio01 <alessio.pesavento@gmail.com>
2021-11-22 20:43:13 +0100

    inserimento funzione fitness

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,4 +1,5 @@
 
+
 #include <iostream>
 #include <Cmath>
 #include <cstdlib>
@@ -10,10 +11,10 @@ int main() {
 
 	srand(time(NULL));
 
-	const int numero_localita = 10; //numero di località
+	const int numero_localita = 10; //numero di localit?
 	const int numero_cromosomi = 5;
 
-//inserimento coordinate delle località e calcolo della matrice con le distanze tra ogni singola località
+	//inserimento coordinate delle localit? e calcolo della matrice con le distanze tra ogni singola localit?
 	int coordinate[numero_localita][2];
 	double distanze[numero_localita][numero_localita];
 
@@ -43,7 +44,7 @@ int main() {
 			int a = rand() % numero_localita;
 			distanze[j][i] = a;
 			distanze[i][j] = a;
-			if (i==j) {
+			if (i == j) {
 				distanze[i][i] = 0;
 			}
 		}
@@ -55,7 +56,7 @@ int main() {
 #if 0
 	for (int i = 0; i < 2; i++) {
 		for (int j = 0; j < numero_localita; j++) {
-			cout<<coordinate[j][i] << " ";
+			cout << coordinate[j][i] << " ";
 		}
 		cout << endl;
 	}
@@ -87,7 +88,7 @@ int main() {
 		cout << endl;
 	}
 
-//Creazione di 5 "cromosomi" random contenenti tutte le località senza ripetizione
+	//Creazione di 5 "cromosomi" random contenenti tutte le localit? senza ripetizione
 
 	int gen1[numero_cromosomi][numero_localita];
 
@@ -95,7 +96,7 @@ int main() {
 		for (int j = 0; j < numero_localita; j++) {
 			gen1[i][j] = rand() % numero_localita; //genero un numero random compreso tra 0 e 9
 			for (int a = 0; a < j; a++) { //controllo che non ci siano ripetizioni
-				if (gen1[i][j]==gen1[i][a]) {
+				if (gen1[i][j] == gen1[i][a]) {
 					j--; //se ci sono ripetizioni torno indietro di una cella e esco dal ciclo, altrimenti vado avanti
 					break;
 				}
@@ -109,12 +110,36 @@ int main() {
 		for (int j = 0; j < numero_localita; j++) {
 			cout << gen1[i][j] << " ";
 		}
-		cout<<endl;
+		cout << endl;
 	}
 
+	//funzione di fitness
 
-	int cromosoma_ricombinante=0;
-	int cromosoma_riferimento=0;
+	double somma = 0;
+	int a_cromosoma_temp[numero_localita];
+	int a;
+	int b;
+	for (int i = 0; i < numero_localita; i++) {
+		a_cromosoma_temp[i] = gen1[0][i];
+	}
+
+	for (int i = 0; i < numero_localita; i++) {
+		if (i < numero_localita - 1) {
+			a = a_cromosoma_temp[i];
+			b = a_cromosoma_temp[i + 1];
+			somma = distanze[a][b] + somma;
+		}
+		else if(i=numero_localita-1){
+			a = a_cromosoma_temp[i];
+			b = a_cromosoma_temp[0];
+			somma = distanze[a][b] + somma;
+		}
+	}
+	cout << "Somma delle distanze: " << somma << endl;
+
+
+	int cromosoma_ricombinante = 0;
+	int cromosoma_riferimento = 0;
 
 	do {
 		cromosoma_ricombinante = rand() % numero_cromosomi;
@@ -129,20 +154,20 @@ int main() {
 		a_cromosoma_riferimento[i] = gen1[cromosoma_riferimento][i];
 	}
 	//selezione del punto di slice
-	int punto_di_slice = 3;
+	int punto_di_slice = 2;/*
 	while (punto_di_slice == 0) {
 		punto_di_slice = rand() % (numero_localita / 2);
-	}
-	
+	}*/
+
 	cout << "punto di slice: ";
 	cout << punto_di_slice << endl;
-	cout << "numero cromosoma ricombinante: "<<endl;
-	cout << cromosoma_ricombinante << endl<< "numero cromosoma di riferimento: " << endl << cromosoma_riferimento << endl;
+	cout << "numero cromosoma ricombinante: " << endl;
+	cout << cromosoma_ricombinante << endl << "numero cromosoma di riferimento: " << endl << cromosoma_riferimento << endl;
 	cout << "cromosoma ricombinante: " << endl;
 	for (int i = 0; i < numero_localita; i++) {
-		cout<<a_cromosoma_ricombinante[i];
+		cout << a_cromosoma_ricombinante[i];
 	}
-	cout <<endl<< "cromosoma di riferimento: " << endl;
+	cout << endl << "cromosoma di riferimento: " << endl;
 	for (int i = 0; i < numero_localita; i++) {
 		cout << a_cromosoma_riferimento[i];
 	}
@@ -158,9 +183,9 @@ int main() {
 	for (int i = 0; i < punto_di_slice; i++) {
 		temp_dx.push_back(a_cromosoma_ricombinante[i]);
 	}
-	
+
 	//trovo l'estremo iniziale del cromosoma
-	for (int i = numero_localita-1; i >= numero_localita - punto_di_slice; i--) {
+	for (int i = numero_localita - 1; i >= numero_localita - punto_di_slice; i--) {
 		vettore_da_ricombinare.push_back(a_cromosoma_ricombinante[i]);
 	}
 
@@ -195,7 +220,7 @@ int main() {
 	vettore_da_ricombinare.insert(vettore_da_ricombinare.end(), temp_dx.begin(), temp_dx.end());
 
 	//stampa vector concatenato
-	cout << "parte iniziale e finale concatenata:  "<<endl;
+	cout << "parte iniziale e finale concatenata:  " << endl;
 	for (int i = 0; i < punto_di_slice * 2; i++) {
 		cout << vettore_da_ricombinare[i];
 	}
@@ -246,4 +271,25 @@ int main() {
 
 	return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
